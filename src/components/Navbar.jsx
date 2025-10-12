e62f8d936a58d4ecd88daddeb403361ca3992cd0
Oluwadunsin david <oluwadunsindavid21@gmail.com>
2025-10-12 07:36:51 +0100

    `feat: updated navbar to link to login page, refactored about page, login page, and orders page with new features and components`

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,11 +51,14 @@ const Navbar = () => {
         /> */}
 
         <div className=" group relative">
-          <img
-            src={assets.profile_icon}
-            className="w-5 cursor-pointer"
-            alt=""
-          />
+          <Link to="/login">
+            {" "}
+            <img
+              src={assets.profile_icon}
+              className="w-5 cursor-pointer"
+              alt=""
+            />
+          </Link>
           {/* DROPDOWN MENU THAT WILL DISPLAY THE LOGIN, LOGOUT, CART PAGE LINK FOR WHEN YOU HOVER ON THE PROFILE ICON*/}
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
